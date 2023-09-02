b716a14d49352744a771973b2fc545313f41ce8b
VincentVillaflores <vdvillaflores@icloud.com>
2023-09-02 17:59:23 +1000

    added a constructor

--- a2/pcb.h
+++ a2/pcb.h
@@ -12,16 +12,23 @@
 class pcb {
     // the unique process id
     osp2023::id_type id;
+
     // the total time that a process / job should run for
     osp2023::time_type total_time;
+
     // the time that has been used so far for this process
     osp2023::time_type time_used;
+
     // the priority level for this process
     // how much time has this process spent waiting for the cpu?
     osp2023::time_type total_wait_time;
+
     // what time was this process last on the cpu?
 
    public:
+    pcb(osp2023::id_type id, osp2023::time_type total_time)
+        : id(id), total_time(total_time), time_used(0), total_wait_time(0) {}
+        
     // max and min duration for a process in our system.
     static constexpr osp2023::time_type MAX_DURATION = 100;
     static constexpr osp2023::time_type MIN_DURATION = 10;
